f4b77a533ae88cd038da5764ca824c1b0015d9ab
Jaison Villanueva <jvill049@bolt.cs.ucr.edu>
2018-01-25 23:00:28 -0800

    Plane Intersection implemented

--- src/plane.cpp
+++ src/plane.cpp
@@ -10,7 +10,23 @@ bool Plane::
 Intersection(const Ray& ray, std::vector<Hit>& hits) const
 {
     // TODO
-    return false;
+    if(dot(normal, ray.direction) == 0)		//Checks denomenator of following t equation. 
+											//If equal to 0, then ray is inside plane, therefore no hit
+    {
+		return false;
+	}
+    double t = dot(normal, x1 - ray.endpoint) / dot(normal, ray.direction);
+    
+    if(t >= 0)			//If t >= 0, then there is a hit, otherwise there is no hit
+    {
+		Hit hit1;
+		hit1.object = this;
+		hit1.t = t;
+		hit1.ray_exiting = false;
+		hits.push_back(hit1);
+		return true;
+	}
+	return false;
 }
 
 vec3 Plane::
